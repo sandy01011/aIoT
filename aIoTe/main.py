99daaac5234bfaa6f98b626c0cf8bba03dbfb06d
sandy01011 <sk2011mishra@gmail.com>
2022-11-24 21:47:55 +0530

    aIoTe

--- aIoTe/main.py
+++ aIoTe/main.py
@@ -1,1 +1,15 @@
+#aIoT-Edge is the device which is going to be the first termination point of data originators like,
+# sensors, audiovisuals etc.
+#aIoTe will have the capability to produce local intelligence to an extent of the compute cost.
+#aIoTe will have the capability to transmit and recieve data to and from datastores like.
+# dataMarts, dataModels (Local or Cloud) with event based frequency where events can be near realtime or scheduled.
+#aIoTe protocol stack list, not limited to,
+# LoRA
+# LPWAN
+# RestAPI
+# MQTT
+# HTTP3
+# QUIC
+# 
+
 print('this is main file to be executed')
