2cc712ee75097ab4abfd2b76110186b9d7614d73
sandy01011 <sk2011mishra@gmail.com>
2022-12-08 13:22:45 +0530

    client mqtt

--- aIoTe/client_mqtt.py
+++ aIoTe/client_mqtt.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqttClient
 import time
-import certifi
 import ssl
+import os
+
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 Connected = False
@@ -9,7 +10,7 @@ username = 'sandeep:gateway-60ssyvgytmzt5u47z7oj1f'
 secret = '390953f2-6884-455e-abff-55ed8c15c90b'
 host = '62.171.143.248'
 port = 8883
-clientID = 'MQTT_1403'
+clientID = os.gethostid
 assetID = '60SsyVGyTmzt5u47Z7Oj1F'
 attribute = 'publish'
 attribute_value = 15
