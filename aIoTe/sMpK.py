bc4c731a9ed898074a957e82849692bfce5e7ecb
sandy01011 <sk2011mishra@gmail.com>
2022-12-17 23:07:55 +0530

    update

--- aIoTe/sMpK.py
+++ aIoTe/sMpK.py
@@ -1,12 +1,12 @@
-# subscribe topic from MQTT broker and publish it to KAFKA broker
-
+# create bridge between MQTT and Kafka broker
+# Subcribe MQTT broker and Publish to Kafka broker
 import paho.mqtt.client as mqtt
 import paho.mqtt.client as mqttClient
 from pykafka import KafkaClient
 import time
 import ssl
 import json
-from envMKP import read_env
+from envSmPk import read_env
 
 
 
@@ -18,10 +18,10 @@ username = env['username']
 secret = env['secret']
 host = env['host']
 port = env['port']
-clientID = 'subMQTTpubKAFKA' # env['clientID']
+clientID = env['clientID']
 assetID = env['assetID']
 topics = env['topics']
-topic = env['topic']
+print('topics {} and type {}'.format(topics, type(topics)))
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("connected to broker")
@@ -30,8 +30,8 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed")
 
-def on_sub_message(client, userdata, message):
-    print('On message')
+def on_message(client, userdata, message):
+    print('On message client {} type {}'.format(client, type(client)))
     #msg_payload = json.loads(message.payload.decode('utf-8'))
     #msg_payload = json.loads(message.payload)
     msg_payload = message.payload
@@ -45,34 +45,48 @@ def on_sub_message(client, userdata, message):
     #print("KAFKA: Just published " + msg_payload + " to topic pub_cam")
     print("MQTT-KAFKA-BKR: Just published data type {} to topic{}:".format(type(msg_payload), topic))
 
-k_topic = realm + '.' + clientID + '.' + assetID + '.' + topic
+#kafka_topic = realm + '.' + clientID + '.' + assetID
 
-try:
-    kafka_client = KafkaClient(hosts="localhost:9092")
-    kafka_topic = kafka_client.topics[k_topic]
+kafka_client = KafkaClient(hosts="localhost:9092")
+#kafka_topic = kafka_client.topics[k_topic]
+#kafka_producer = kafka_topic.get_sync_producer()
+
+#clientMQTT = mqttClient.Client(clientID)
+#clientMQTT.username_pw_set(username, password=secret)
+#clientMQTT.tls_set_context(context)
+#clientMQTT.on_connect = on_connect
+# clientMQTT.on_publish = on_publish
+#clientMQTT.on_message = on_message
+#clientMQTT.connect(host, port=port)
+#clientMQTT.loop_start()
+
+#while Connected != True:
+#    time.sleep(0.1)
+#
+def subpub(topic):
+    print('inside for topic loop with topic:', topic)
+    kafka_clientID = clientID + '.' + topic
+    mqtt_clientID = clientID + '_' + topic
+    kafka_topic = realm + '.' + assetID + '.' + kafka_clientID
+    kafka_topic = kafka_client.topics[kafka_topic]
     kafka_producer = kafka_topic.get_sync_producer()
-except:
-    print('failed to connect kafka broker')
-try:
-    clientMQTT = mqttClient.Client(clientID)
+    clientMQTT = mqttClient.Client(mqtt_clientID)
     clientMQTT.username_pw_set(username, password=secret)
     clientMQTT.tls_set_context(context)
     clientMQTT.on_connect = on_connect
-    # clientMQTT.on_publish = on_publish
-    clientMQTT.on_message = on_sub_message
+# clientMQTT.on_publish = on_publish
+    clientMQTT.on_message = on_message
     clientMQTT.connect(host, port=port)
     clientMQTT.loop_start()
-
-    while Connected != True:
-        time.sleep(0.1)
-
     try:
-        #clientMQTT.subscribe(f'{realm}/{clientID}/attribute/{pub_cam}/{assetID}')
-        clientMQTT.subscribe(f'{realm}/{clientID}/attribute/{topic}/{assetID}')
+        clientMQTT.subscribe(f'{realm}/{mqtt_clientID}/attribute/{topic}/{assetID}')
     except:
         print('failed to sbscribe')
-except:
-    print('failed to connect mqtt broker')
+
+
+while Connected != True:
+    time.sleep(0.1)
+
 
 try:
     while True:
